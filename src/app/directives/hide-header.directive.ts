ccecf50ddf383533625221bcc5f9c86e1d83652c
doyedele1 <demilade.oyedele@gmail.com>
2021-01-30 00:55:50 +0100

    making changes

--- src/app/directives/hide-header.directive.ts
+++ src/app/directives/hide-header.directive.ts
@@ -1,10 +1,38 @@
-import { Directive } from '@angular/core';
+import { AfterViewInit, Directive, HostListener, Input, Renderer2 } from '@angular/core';
+import { DomController, isPlatform } from '@ionic/angular';
 
 @Directive({
   selector: '[appHideHeader]'
 })
-export class HideHeaderDirective {
+export class HideHeaderDirective implements AfterViewInit {
+  @Input('appHideHeader') header: any;
+  private headerHeight = isPlatform('ios') ? 44 : 56;
+  private children: any;
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private domCtrl: DomController) { }
 
+  ngAfterViewInit(): void {
+    this.header = this.header.el;
+    this.children = this.header.children;
+    // console.log('header', this.header);
+    // console.log('children', this.children);
+  }
+
+  @HostListener('ionScroll', ['$event']) onContentScroll($event: any) {
+    const scrollTop: number = $event.detail.scrollTop;
+    let newPosition = -scrollTop;
+
+    if (newPosition < -this.headerHeight) {
+      newPosition = -this.headerHeight;
+    }
+
+    const newOpactiy = 1 - (newPosition / -this.headerHeight);
+
+    this.domCtrl.write(() => {
+      this.renderer.setStyle(this.header, 'top', newPosition + 'px');
+      for (const c of this.children) {
+        this.renderer.setStyle(c, 'opacity', newOpactiy);
+      }
+    });
+  }
 }
--- src/app/directives/sticky-segment.directive.ts
+++ src/app/directives/sticky-segment.directive.ts
@@ -1,10 +1,30 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, Input, Renderer2 } from '@angular/core';
+import { DomController, isPlatform } from '@ionic/angular';
 
 @Directive({
   selector: '[appStickySegment]'
 })
 export class StickySegmentDirective {
+  @Input('appStickySegment') segment: any;
+  private headerHeight = isPlatform('ios') ? 44 : 56;
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private domCtrl: DomController) { }
 
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngAfterViewInit(): void {
+    this.segment = this.segment.el;
+  }
+
+  @HostListener('ionScroll', ['$event']) onContentScroll($event: any) {
+    const scrollTop: number = $event.detail.scrollTop;
+    let newPosition = -scrollTop;
+
+    if (newPosition < -this.headerHeight) {
+      newPosition = -this.headerHeight;
+    }
+
+    this.domCtrl.write(() => {
+      this.renderer.setStyle(this.segment, 'top', newPosition + 'px');
+    });
+  }
 }
